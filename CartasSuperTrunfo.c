2ad907a2dd0966b60943caf714a516a414cc7b86
Cesar090620 <168016246+Cesar090620@users.noreply.github.com>
2025-03-07 03:16:25 +0000

    Etapa de Cadastro e retorno- Concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,16 +4,86 @@ int main() {
    
 printf("Cadastro de cartas do Supertrunfo: \n");
     
-    char estado[50]= "santos";
+    char estado[50]= "SP";
     char codigoEstado [10]="A1";
     char NomeCidade [50]="Fortaleza";
     int população = 200000;
     float PIB = 120000.00;
     int PontoTuristico = 35;
 
-    printf ("Digite as informações da sua carta: \n");
-    printf ("Digite o Nome da cidade: \n");
+    printf ("Digite as informações da sua carta 01: \n");
+    
+    printf ("Digite o Nome da cidade: \n", NomeCidade);
     scanf ("%s", &NomeCidade);
+
+    printf("Digite o nome do estado: \n",estado);
+    scanf("%s", &estado);
+
+    printf ("Digite o codigo do estado: \n", codigoEstado);
+    scanf ("%s", &codigoEstado);
+
+    printf ("Digite o numero populacional: \n", população);
+    scanf ("%d", &população);
+
+    printf ("Digite o PIB do seu estado: \n", PIB);
+    scanf ("%i", &PIB);
+
+    printf ("Digite a quantidade de pontos turisticos da sua carta: \n", PontoTuristico);
+    scanf ("%d", &PontoTuristico);
+
+    printf ("Carta 1: \n");
+    printf("Nome da cidade: %s \n", NomeCidade);
+    printf ("Estado: %s \n", estado);
+    printf("Codigo do Estado: %s \n", codigoEstado);
+    printf("População: %d \n", população);
+    printf("PIB: %i \n", PIB);
+    printf("Quantidade de Pontos Turisticos: %d \n", PontoTuristico);
+
+
+
+    printf ("Digite as informações da sua carta 02: \n");
+    
+    printf ("Digite o Nome da cidade: \n", NomeCidade);
+    scanf ("%s", &NomeCidade);
+
+    printf("Digite o nome do estado: \n",estado);
+    scanf("%s", &estado);
+
+    printf ("Digite o codigo do estado: \n", codigoEstado);
+    scanf ("%s", &codigoEstado);
+
+    printf ("Digite o numero populacional: \n", população);
+    scanf ("%d", &população);
+
+    printf ("Digite o PIB do seu estado: \n", PIB);
+    scanf ("%i", &PIB);
+
+    printf ("Digite a quantidade de pontos turisticos da sua carta: \n", PontoTuristico);
+    scanf ("%d", &PontoTuristico);
+
+    printf ("Carta 2: \n");
+    printf("Nome da cidade: %s \n", NomeCidade);
+    printf ("Estado: %s \n", estado);
+    printf("Codigo do Estado: %s \n", codigoEstado);
+    printf("População: %d \n", população);
+    printf("PIB: %i \n", PIB);
+    printf("Quantidade de Pontos Turisticos: %d \n", PontoTuristico);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
