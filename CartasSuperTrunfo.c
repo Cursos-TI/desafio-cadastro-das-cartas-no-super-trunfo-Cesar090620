16d8f9e53eb71a39a11d6ad4cb1d5b70d2897458
Cesar090620 <168016246+Cesar090620@users.noreply.github.com>
2025-03-11 02:02:18 +0000

    Adição de parametros Densidade populacional, PIB per capita e área. Correção de erros de unidade.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,6 +10,13 @@ printf("Cadastro de cartas do Supertrunfo: \n");
     int população = 200000;
     float PIB = 120000.00;
     int PontoTuristico = 35;
+    float area=1000.00;  
+    float DensidadePopulacional=0.00;
+    float PibPerCapita=0.00;
+
+
+    float divisão1, divisão2; 
+
 
     printf ("Digite as informações da sua carta 01: \n");
     
@@ -25,19 +32,28 @@ printf("Cadastro de cartas do Supertrunfo: \n");
     printf ("Digite o numero populacional: \n", população);
     scanf ("%d", &população);
 
+
     printf ("Digite o PIB do seu estado: \n", PIB);
     scanf ("%i", &PIB);
 
     printf ("Digite a quantidade de pontos turisticos da sua carta: \n", PontoTuristico);
     scanf ("%d", &PontoTuristico);
+    
+    divisão1= população/ area;
+    divisão2= PIB/população;
+
+
 
     printf ("Carta 1: \n");
     printf("Nome da cidade: %s \n", NomeCidade);
     printf ("Estado: %s \n", estado);
     printf("Codigo do Estado: %s \n", codigoEstado);
     printf("População: %d \n", população);
-    printf("PIB: %i \n", PIB);
+    printf("PIB: bilhões de reais %f \n", PIB);
+    printf("A area da sua cidade é de: %.2f km² \n", area);
     printf("Quantidade de Pontos Turisticos: %d \n", PontoTuristico);
+    printf("Densidade Populacional: hab/km² %.2f \n", divisão1);
+    printf("PIB per Capita: reais %.2f \n", divisão2);
 
 
 
@@ -58,16 +74,28 @@ printf("Cadastro de cartas do Supertrunfo: \n");
     printf ("Digite o PIB do seu estado: \n", PIB);
     scanf ("%i", &PIB);
 
+    printf ("Digite a área da cidade: \n", area);
+     scanf ( "%f", &area);
+
     printf ("Digite a quantidade de pontos turisticos da sua carta: \n", PontoTuristico);
     scanf ("%d", &PontoTuristico);
+
+    divisão1= população/ area;
+    divisão2= PIB/população;
+
+
 
     printf ("Carta 2: \n");
     printf("Nome da cidade: %s \n", NomeCidade);
     printf ("Estado: %s \n", estado);
     printf("Codigo do Estado: %s \n", codigoEstado);
     printf("População: %d \n", população);
-    printf("PIB: %i \n", PIB);
+    printf("PIB: bilhões de reais %f \n", PIB);
+    printf("A area da sua cidade é de: %.2f km² \n", area);
     printf("Quantidade de Pontos Turisticos: %d \n", PontoTuristico);
+    printf("Densidade Populacional: hab/km² %.2f \n", divisão1);
+    printf("PIB per Capita: reais %.2f \n", divisão2);
+
 
 
 
@@ -92,7 +120,7 @@ printf("Cadastro de cartas do Supertrunfo: \n");
     
     
     
-    
+    return 0; 
     
     
 }
